793e9fc47dca9b28411d076f9a0f54a782a875f8
shokhruh61 <shohruxmahmudov61@gmail.com>
2025-03-31 22:15:55 +0500

    enhance Home component with error handling and improved search input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import Masonry from "react-masonry-css";
 import { useFetch } from "../hooks/useFetch";
 import ImageCard from "../components/ImageCard";
-import FormInput from "../components/FormInput";
 
 function Home() {
   const navigate = useNavigate();
@@ -13,7 +12,7 @@ function Home() {
   const [page, setPage] = useState(1);
   const [search, setSearch] = useState("");
 
-  const { data, isPending } = useFetch(
+  const { data, isPending, error } = useFetch(
     `https://api.unsplash.com/search/photos?client_id=TfijlH2P6tfgB_kzepCuV_-ji3Pd7waZSo1Gx4SDBbE&query=${searchQuery}&page=${page}`,
   );
 
@@ -37,29 +36,30 @@ function Home() {
 
   return (
     <div className="container mx-auto px-4">
+      {/* Search Form */}
       <div className="my-5 flex justify-center">
         <form
           onSubmit={handleSubmit}
           className="flex w-full max-w-96 items-center gap-2"
         >
-          <FormInput
+          <input
             type="text"
             placeholder="Search..."
             name="search"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
+            className="input-bordered input w-full"
           />
-          <button
-            type="submit"
-            className={`btn btn-secondary btn-sm md:hidden`}
-          >
+          <button type="submit" className="btn btn-primary btn-sm md:hidden">
             Search
           </button>
         </form>
       </div>
 
       {isPending && <h1 className="text-center text-xl">Loading...</h1>}
+      {error && <div className="text-center text-red-500">Error: {error}</div>}
 
+      {/* Masonry Grid */}
       <Masonry
         breakpointCols={{ default: 4, 1024: 3, 768: 2, 576: 1 }}
         className="flex gap-4"
@@ -70,10 +70,11 @@ function Home() {
         ))}
       </Masonry>
 
+      {/* Load More Button */}
       <div className="my-10 flex justify-center">
         <button
           onClick={() => setPage((p) => p + 1)}
-          className={`btn btn-primary w-full max-w-full px-4 py-2 text-xl`}
+          className="btn btn-primary max-w-md px-4 py-2 text-xl"
         >
           Load More
         </button>
